4f41d5ab2f2c16e41c4d7a4609fb6d5936a8d477
Ole Martin Kristiansen <olemak@apasje.no>
2018-04-06 23:43:58 +0200

    Basic functionality/logic OK

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("loaded");
+(() => {
+    function fn() {
+        init();
+    }
+    if (document.readyState != "loading") {
+        fn();
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+})();
+
+function init() {
+    const age1_input = window.age1;
+    const age2_input = window.age2;
+
+    age1_input.addEventListener("change", inputChange);
+    age2_input.addEventListener("change", inputChange);
+}
+
+function inputChange(event) {
+    calculator[event.target.id] = parseInt(event.target.value);
+    window.output.innerHTML = calculator.ratio;
+}
+
+const calculator = {
+    ageOne: 20,
+    ageTwo: 30,
+    set age1(value) {
+        this.ageOne = value;
+    },
+    set age2(value) {
+        this.ageTwo = value;
+    },
+    get ratio() {
+        const ages = [this.ageOne, this.ageTwo].sort();
+        const lowestAge = ages[0];
+        const highestAge = ages[1];
+        const half = highestAge / 2;
+        const minimumAge = half + 7;
+        return lowestAge >= minimumAge;
+    }
+};
